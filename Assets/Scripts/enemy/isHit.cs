02306cb9460058f1f3612008e9173fc00c00a260
baoqifan <baoqifan2022@163.com>
2022-04-06 18:52:29 +0800

    爆炸效果

--- Assets/Scripts/enemy/isHit.cs
+++ Assets/Scripts/enemy/isHit.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class isHit : MonoBehaviour
 {
+    public GameObject baozha;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("+++++++++++++++++++++++++++++++++++++++++++++++");
         if (collision.gameObject.CompareTag("bul"))  //发生碰撞子弹
         {
-
+            var newBullet = Instantiate(baozha);
+            newBullet.transform.position = this.transform.position;
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
+            Destroy(newBullet, 0.1f);
+            
 
         }
     }
